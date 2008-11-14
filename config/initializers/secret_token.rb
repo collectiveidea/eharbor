c2afd905841b918d14bab71f45fe9552fda1788b
Brandon Keepers <brandon@collectiveidea.com>
2008-11-14 11:14:09 -0500

    New Rails project
    rails new eharbor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eharbor::Application.config.secret_token = '0aab08031d1d90768b6f04c6c814128c5e1bfc82033302f653b19d5c1a93209e8557aa6806a25180eee5063d9d97269e81a6a7e2f6a9aac0c0bf9872fc7a107f'
