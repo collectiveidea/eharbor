6c78e404764a6d9ac592c6add3f5a67028df370e
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 01:10:30 -0500

    Add associations between User and Listing

--- test/unit/listing_test.rb
+++ test/unit/listing_test.rb
@@ -3,5 +3,5 @@ require 'test_helper'
 class ListingTest < ActiveSupport::TestCase
   
   should_validate_presence_of :title, :description
-  
+  should_belong_to :user
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should_have_many :listings
+
   should_validate_presence_of :nickname
   should_validate_uniqueness_of :nickname, :case_sensitive => false
   should_allow_mass_assignment_of :nickname
