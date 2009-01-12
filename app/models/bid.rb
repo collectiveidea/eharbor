b467b4a677501457b946a48d13c1939f3c6056af
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 11:06:17 -0500

    Validations on bid and tests

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,4 +3,16 @@ class Bid < ActiveRecord::Base
   belongs_to :listing
   
   money_column :amount
+  
+  validates_presence_of :user_id, :listing_id, :amount
+  validate :verify_minimum_bid
+  
+private
+  
+  def verify_minimum_bid
+    if listing.highest_bid && amount && listing.highest_bid.amount >= amount
+      errors.add :amount, 'must be more than highest bid.'
+    end
+  end
+  
 end
