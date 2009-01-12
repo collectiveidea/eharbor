3c830818cc6560809c056c5d0d3c438d55a7bbf5
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 11:06:17 -0500

    Validations on bid and tests

--- test/unit/bid_test.rb
+++ test/unit/bid_test.rb
@@ -3,4 +3,27 @@ require 'test_helper'
 class BidTest < ActiveSupport::TestCase
   should_belong_to :user
   should_belong_to :listing
+  
+  context "highest bid validations" do
+    setup do
+      listings(:macbook).bids.create! :amount => 100, :user => users(:daniel)
+    end
+    
+    should "be invalid when lower than highest bid" do
+      bid = listings(:macbook).bids.build :amount => 99.99, :user => users(:daniel)
+      assert !bid.valid?
+      assert_not_nil bid.errors.on(:amount)
+    end
+    
+    should "be invalid when equal to highest bid" do
+      bid = listings(:macbook).bids.build :amount => 100, :user => users(:daniel)
+      assert !bid.valid?
+      assert_not_nil bid.errors.on(:amount)
+    end
+    
+    should "be valid when greater than highest bid" do
+      bid = listings(:macbook).bids.build :amount => 100.01, :user => users(:daniel)
+      assert bid.valid?
+    end
+  end
 end
