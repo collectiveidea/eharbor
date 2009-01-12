31d2c180a325be64f7fc389b4b3093dc8720ed65
Brandon Keepers <brandon@collectiveidea.com>
2009-01-11 22:45:54 -0500

    validates_presence_of :title, :body

--- test/unit/listing_test.rb
+++ test/unit/listing_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def valid_listing_attrs(attrs = {})
+    {:title => 'Title', :description => 'Description'}.merge(attrs)
   end
+
+  test "is invalid without a title" do
+    listing = Listing.new valid_listing_attrs(:title => nil)
+    assert !listing.valid?
+    listing.title = 'A Listing'
+    assert listing.valid?
+  end
+
+  test "is invalid without a description" do
+    listing = Listing.new valid_listing_attrs(:description => nil)
+    assert !listing.valid?
+    listing.description = 'A description'
+    assert listing.valid?
+  end
+
 end
