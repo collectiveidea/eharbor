2ca799f746019c30959aed122c92eb91c560bb4b
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 10:49:58 -0500

    Added collectiveidea-money gem.

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem  'clearance', :version => '0.8.8'
+  config.gem 'collectiveidea-money', :lib => 'money/rails', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
