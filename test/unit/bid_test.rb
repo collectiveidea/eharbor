c5b756831f23114324f2bf578559e5e4140958c2
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 12:02:13 -0500

    Use callback to set end_at based on duration

--- test/unit/bid_test.rb
+++ test/unit/bid_test.rb
@@ -5,7 +5,7 @@ class BidTest < ActiveSupport::TestCase
   def setup
     listings(:macbook).bids.create! :amount => 100, :user => users(:daniel)
   end
-    
+  
   test "highest bid should be invalid when lower than highest bid" do
     bid = listings(:macbook).bids.build :amount => 99.99, :user => users(:daniel)
     assert !bid.valid?
@@ -21,5 +21,12 @@ class BidTest < ActiveSupport::TestCase
   test "highest bid should be valid when greater than highest bid" do
     bid = listings(:macbook).bids.build :amount => 100.01, :user => users(:daniel)
     assert bid.valid?
+  end
+
+  test "should be invalid when listing is expired" do
+    listings(:macbook).end_at = 5.seconds.ago
+    listings(:macbook).save!
+    bid = listings(:macbook).bids.build :amount => 100, :user => users(:daniel)
+    assert !bid.valid?
   end
 end
