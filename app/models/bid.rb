1b4feb374fe1fe43ee25d160ed15705b52a1a4c3
Brandon Keepers <brandon@collectiveidea.com>
2009-01-12 12:02:13 -0500

    Use callback to set end_at based on duration

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,13 +4,18 @@ class Bid < ActiveRecord::Base
 
   validates :user_id, :listing_id, :amount, :presence => true, :numericality => true
   validate :verify_minimum_bid
-
+  validate :verify_active_listing
+  
 private
 
   def verify_minimum_bid
     if listing.highest_bid && amount && listing.highest_bid.amount >= amount
       errors.add :amount, 'must be more than highest bid.'
     end
+  end
+  
+  def verify_active_listing
+    errors.add :base, 'This listing has expired.' unless listing.active?
   end
   
 end
