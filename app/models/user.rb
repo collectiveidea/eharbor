a378d8767b13f5fdf88adfa1473aca96950542e5
Brandon Keepers <brandon@collectiveidea.com>
2009-06-05 14:45:22 -0400

    Added nickname to signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  validates_presence_of :nickname
+  validates_uniqueness_of :nickname, :case_sensitive => false
+  
+  attr_accessible :nickname
+  
+  def to_s
+    nickname
+  end
 end
