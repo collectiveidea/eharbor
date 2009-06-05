c512edc34d1f6300ac8bf15c2157c03cb97550f2
Brandon Keepers <brandon@collectiveidea.com>
2009-06-05 14:23:16 -0400

    script/generate clearance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  Clearance::Routes.draw(map)
   map.resources :listings
   
   # The priority is based upon order of creation: first created -> highest priority.
