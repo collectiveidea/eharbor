ab077ed1f51b308b21079464eaf46aa503293085
Brandon Keepers <brandon@collectiveidea.com>
2009-06-05 15:15:18 -0400

    Added profile page for users that shows their listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :listings
+  map.resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
 
