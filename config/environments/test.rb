98c53b56f7cad016d0ccfbe18e209bab5798bb23
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 17:40:32 -0400

    Added factory_girl dependency

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,3 +24,6 @@ config.action_mailer.delivery_method = :test
 config.gem 'thoughtbot-shoulda',
   :source => 'http://gems.github.com',
   :lib => 'shoulda'
+config.gem 'thoughtbot-factory_girl',
+  :source => 'http://gems.github.com',
+  :lib => 'factory_girl'
