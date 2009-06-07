79b972a142cb7bc8c487f8fc2e7fbd1cbc68d581
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 13:48:24 -0400

    Switch to using fixtures

--- test/functional/listings_controller_test.rb
+++ test/functional/listings_controller_test.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class ListingsControllerTest < ActionController::TestCase
   
   test "index" do
-    listing = Listing.create! :title => 'Foo', :description => 'Bar'
-    
     get :index
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:listings)
-    assert assigns(:listings).include?(listing)
+    assert assigns(:listings).include?(listings(:macbook))
   end
   
 end
