8812b54fa559da2a49d09d9ac9d77737fd1bd005
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 10:55:24 -0400

    Use attr_accessible

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,7 @@ class Listing < ActiveRecord::Base
   
   before_save :calculate_end_at
   
-  attr_protected :end_at
+  attr_accessible :title, :description, :duration
   
   named_scope :active, lambda {{
     :conditions => ['listings.created_at <= :now AND listings.end_at >= :now', {:now => Time.zone.now}],
