57d7d8805020af92985f88ca39d2e3b61df08457
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 16:02:20 -0400

    Convert model test to shoulda

--- test/unit/listing_test.rb
+++ test/unit/listing_test.rb
@@ -1,23 +1,7 @@
 require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-
-  def valid_listing_attrs(attrs = {})
-    {:title => 'Title', :description => 'Description'}.merge(attrs)
-  end
-
-  test "is invalid without a title" do
-    listing = Listing.new valid_listing_attrs(:title => nil)
-    assert !listing.valid?
-    listing.title = 'A Listing'
-    assert listing.valid?
-  end
-
-  test "is invalid without a description" do
-    listing = Listing.new valid_listing_attrs(:description => nil)
-    assert !listing.valid?
-    listing.description = 'A description'
-    assert listing.valid?
-  end
-
+  
+  should_validate_presence_of :title, :description
+  
 end
