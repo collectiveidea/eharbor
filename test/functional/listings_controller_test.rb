d4a4844e4e2f0f127ac1c350366234d61ee03f2f
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 15:15:29 -0400

    Tests for ListingsController #new and #create

--- test/functional/listings_controller_test.rb
+++ test/functional/listings_controller_test.rb
@@ -21,4 +21,27 @@ class ListingsControllerTest < ActionController::TestCase
     assert_select 'h2', listings(:macbook).title
   end
   
+  test "new" do
+    get :new
+    assert_response :success
+    assert_template 'new'
+    assert_not_nil assigns(:listing)
+    
+    assert_select 'form[action=?]', listings_path do
+      assert_select 'input[type=text][name=?]', 'listing[title]'
+      assert_select 'label[for=?]', 'listing_title'
+      assert_select 'label[for=?]', 'listing_description'
+      assert_select 'textarea[name=?]', 'listing[description]'
+      assert_select 'input[type=submit]'
+    end
+  end
+  
+  test "create" do
+    assert_difference 'Listing.count' do
+      post :create, :listing => {:title => 'Getting Testy', :description => "Lorem..."}
+    end
+    assert_response :redirect
+    assert_redirected_to listing_path(assigns(:listing))
+  end
+  
 end
