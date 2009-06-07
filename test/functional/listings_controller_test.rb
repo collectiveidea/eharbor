e758038e095562edad09351ceb47c351c8a81020
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 16:00:54 -0400

    Convert tests for ListingsController to shoulda

--- test/functional/listings_controller_test.rb
+++ test/functional/listings_controller_test.rb
@@ -16,51 +16,71 @@ class ListingsControllerTest < ActionController::TestCase
     end
   end
   
-  test "show" do
-    get :show, :id => listings(:macbook)
-    assert_response :success
-    assert_template 'show'
-    assert_equal listings(:macbook), assigns(:listing)
+  context "show" do
+    setup do
+      get :show, :id => listings(:macbook)
+    end
     
-    assert_select 'h2', listings(:macbook).title
-  end
-  
-  test "new" do
-    get :new
-    assert_response :success
-    assert_template 'new'
-    assert_not_nil assigns(:listing)
+    should_respond_with :success
+    should_render_template 'show'
+    should_assign_to(:listing) { assigns(:listing) }
     
-    assert_select 'form[action=?]', listings_path do
-      assert_select 'input[type=text][name=?]', 'listing[title]'
-      assert_select 'label[for=?]', 'listing_title'
-      assert_select 'label[for=?]', 'listing_description'
-      assert_select 'textarea[name=?]', 'listing[description]'
-      assert_select 'input[type=submit]'
+    should "should listing title" do
+      assert_select 'h2', listings(:macbook).title
     end
   end
   
-  test "create" do
-    assert_difference 'Listing.count' do
+  context "new" do
+    setup do
+      get :new
+    end
+    
+    should_respond_with :success
+    should_render_template 'new'
+    should_assign_to :listing
+    should "render form" do
+      assert_select 'form[action=?]', listings_path do
+        assert_select 'input[type=text][name=?]', 'listing[title]'
+        assert_select 'label[for=?]', 'listing_title'
+        assert_select 'label[for=?]', 'listing_description'
+        assert_select 'textarea[name=?]', 'listing[description]'
+        assert_select 'input[type=submit]'
+      end
+    end
+  end
+  
+  context "create" do
+    setup do
       post :create, :listing => {:title => 'Getting Testy', :description => "Lorem…"}
     end
-    assert_response :redirect
-    assert_redirected_to listing_path(assigns(:listing))
+    
+    should_change 'Listing.count'
+    should_respond_with :redirect
+    should_redirect_to('the listing') { listing_path(assigns(:listing)) }
   end
   
-  test "edit" do
-    get :edit, :id => listings(:macbook)
-    assert_response :success
-    assert_template 'edit'
-    assert_equal listings(:macbook), assigns(:listing)
+  context "edit" do
+    setup do
+      get :edit, :id => listings(:macbook)
+    end
+    
+    should_respond_with :success
+    should_render_template 'edit'
+    should_assign_to(:listing) { listings(:macbook) }
   end
   
-  test "update" do
-    post :update, :id => listings(:macbook), :listing => {:title => 'Updated'}
-    assert_response :redirect
-    assert_redirected_to listing_path(listings(:macbook))
-    listings(:macbook).reload
-    assert_equal 'Updated', listings(:macbook).title
+  context "update" do
+    setup do
+      post :update, :id => listings(:macbook), :listing => {:title => 'Updated'}
+    end
+    
+    should_respond_with :redirect
+    should_redirect_to('the listing') { listings(:macbook) }
+    
+    should "update the listing" do
+      listings(:macbook).reload
+      assert_equal 'Updated', listings(:macbook).title
+    end
   end
   
 end
