0e42e4d18881efe1f16e8d7190bd3f21aca0855f
Brandon Keepers <brandon@collectiveidea.com>
2009-06-06 22:00:39 -0400

    Added duration and validates inclusion

--- test/unit/listing_test.rb
+++ test/unit/listing_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
   should_validate_presence_of :title, :description
+  should_allow_values_for :duration, 3, 5, 7
+  should_not_allow_values_for :duration, nil, 0, 1, 8, :message => //
   should_belong_to :user
   should_have_many :bids
   
