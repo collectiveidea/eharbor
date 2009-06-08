c1bad0c704de10437b44ef3f1078049716250a74
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 18:39:14 -0400

    Added Time Zone support

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -6,5 +6,5 @@ class UserTest < ActiveSupport::TestCase
 
   should_validate_presence_of :nickname
   should_validate_uniqueness_of :nickname, :case_sensitive => false
-  should_allow_mass_assignment_of :nickname
+  should_allow_mass_assignment_of :nickname, :time_zone
 end
