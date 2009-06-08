a8bc7a65d792be5465f7ac06245fbb504c7b96b0
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:47:11 -0400

    Refactor outbid email to an observer.

--- app/models/bid_observer.rb
+++ app/models/bid_observer.rb
@@ -1,2 +1,9 @@
 class BidObserver < ActiveRecord::Observer
+  
+  def after_create(bid)
+    if previous_bid = bid.listing.bids.first(:order => 'bids.amount DESC', :offset => 1)
+      BidMailer.outbid_notification(previous_bid).deliver
+    end
+  end
+  
 end
--- test/unit/bid_observer_test.rb
+++ test/unit/bid_observer_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class BidObserverTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  setup do
+    ActionMailer::Base.deliveries = []
+    @listing = listings(:macbook)
+    listings(:macbook).bids.destroy_all
   end
+  
+  test "it should send an email if there is a previous bid" do
+    @bid = listings(:macbook).bids.build(:amount => 200)
+    @bid.user = users(:amy)
+    @bid.save!
+    new_bid = listings(:macbook).bids.build(:amount => 300)
+    new_bid.user = users(:amy)
+    new_bid.save!
+    assert !ActionMailer::Base.deliveries.empty?
+  end
+  
+  test "it should not send an email if it is the first bid" do
+    listings(:macbook).bids.destroy_all
+    bid = listings(:macbook).bids.build(:amount => 200)
+    bid.user = users(:amy)
+    bid.save!
+    assert ActionMailer::Base.deliveries.empty?
+  end
+  
 end
