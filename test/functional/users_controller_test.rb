86cc8a5182d54080e176e4392ad416f402afab90
Brandon Keepers <brandon@collectiveidea.com>
2009-06-07 20:27:22 -0400

    Replace fixtures with factories

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -5,15 +5,17 @@ class UsersControllerTest < ActionController::TestCase
   context "not signed in" do
     context "show" do
       setup do
-        get :show, :id => users(:brandon)
+        @user = Factory(:user)
+        @listing = Factory(:listing, :user => @user)
+        get :show, :id => @user
       end
     
       should_respond_with :success
       should_render_template 'show'
-      should_assign_to(:user) { users(:brandon) }
+      should_assign_to(:user) { @user }
     
       should "link to listings" do
-        assert_select 'a[href=?]', listing_path(listings(:macbook))
+        assert_select 'a[href=?]', listing_path(@listing)
       end
     
       should "not have link to edit profile" do
@@ -24,12 +26,13 @@ class UsersControllerTest < ActionController::TestCase
   
   context "signed in" do
     setup do
-      sign_in_as users(:brandon)
+      @user = Factory(:user)
+      sign_in_as @user
     end
     
     context "show" do
       setup do
-        get :show, :id => users(:brandon)
+        get :show, :id => @user
       end
       
       should "not link to edit profile" do
@@ -39,21 +42,21 @@ class UsersControllerTest < ActionController::TestCase
     
     context "edit" do
       setup do
-        get :edit, :id => users(:brandon)
+        get :edit, :id => @user
       end
       
       should_respond_with :success
       should_render_template 'edit'
-      should_assign_to(:user) { users(:brandon) }
+      should_assign_to(:user) { @user }
     end
     
     context "update" do
       setup do
-        put :update, :id => users(:brandon), :user => {:email => 'brandon@example.com'}
+        put :update, :id => @user, :user => {:email => 'brandon@example.com'}
       end
       
       should_respond_with :redirect
-      should_redirect_to('the user profile') { user_path(users(:brandon)) }
+      should_redirect_to('the user profile') { user_path(@user) }
     end
   end
   
