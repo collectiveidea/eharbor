007303b3ef16e74152e79b072b765f87063f15e1
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:47:11 -0400

    Refactor outbid email to an observer.

--- test/unit/bid_observer_test.rb
+++ test/unit/bid_observer_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class BidObserverTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "after_create" do
+    setup do
+      ActionMailer::Base.deliveries = []
+      @listing = Factory(:listing)
+    end
+    
+    should "send an email if there is a previous bid" do
+      Factory(:bid, :amount => 10, :listing => @listing)
+      Factory(:bid, :amount => 12, :listing => @listing)
+      assert_sent_email
+    end
+    
+    should "not send an email if it is the first bid" do
+      Factory(:bid, :amount => 10, :listing => @listing)
+      assert_did_not_send_email
+    end
   end
+  
 end
