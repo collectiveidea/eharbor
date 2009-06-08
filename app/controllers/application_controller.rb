9b317ea5081d7b8be14d6ed3e66c45a880cf64b7
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 18:39:14 -0400

    Added Time Zone support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,11 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+  
+  before_filter :set_time_zone
+
+private
+  def set_time_zone
+    Time.zone = current_user.time_zone if current_user
+  end
 end
