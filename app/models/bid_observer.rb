ba68685ce5764d93f6468f3f51ea6b43003cab66
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:47:11 -0400

    Refactor outbid email to an observer.

--- app/models/bid_observer.rb
+++ app/models/bid_observer.rb
@@ -1,2 +1,9 @@
 class BidObserver < ActiveRecord::Observer
+  
+  def after_create(bid)
+    if previous_bid = bid.listing.bids.first(:order => 'bids.cents DESC', :offset => 1)
+      BidMailer.deliver_outbid_notification(previous_bid)
+    end
+  end
+  
 end
--- test/unit/bid_observer_test.rb
+++ test/unit/bid_observer_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class BidObserverTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def setup
+    ActionMailer::Base.deliveries = []
+    @listing = Factory(:listing)
   end
+  
+  test "after_create should send an email if there is a previous bid" do
+    bid1 = Factory(:bid, :amount => 10, :listing => @listing)
+    bid2 = Factory(:bid, :amount => 12, :listing => @listing)
+    email = ActionMailer::Base.deliveries.detect{|d| d.to.include?(bid1.user.email)}
+    assert_not_nil email
+  end
+  
+  test "after_create should not send an email if it is the first bid" do
+    bid = Factory(:bid, :amount => 10, :listing => @listing)
+    email = ActionMailer::Base.deliveries.detect{|d| d.to.include?(bid.user.email)}
+    assert_nil email
+  end
+  
 end
