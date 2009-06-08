414aa5f5847696a5b90b4559ab4db0d406cf88de
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:35:11 -0400

    Added mailer for outdbid notifications

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -6,6 +6,9 @@ class BidsController < ApplicationController
     @bid = @listing.bids.build(params[:bid])
     @bid.user = current_user
     if @bid.save
+      if previous_bid = @listing.bids.first(:order => 'bids.amount DESC', :offset => 1)
+        BidMailer.outbid_notification(previous_bid).deliver
+      end
       redirect_to @listing
     else
       render 'listings/show'
