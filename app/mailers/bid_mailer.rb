e2abd91a60c54e1999ae4615de8b9820ed1c6965
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:35:11 -0400

    Added mailer for outdbid notifications

--- app/mailers/bid_mailer.rb
+++ app/mailers/bid_mailer.rb
@@ -1,3 +1,9 @@
 class BidMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "eHarbor <app@example.com>"
+  
+  def outbid_notification(bid)
+    @bid = bid
+    mail(:to => bid.user.email,
+         :subject => "You have been outbid")
+  end
 end
