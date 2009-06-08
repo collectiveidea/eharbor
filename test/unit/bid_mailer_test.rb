f488430c6ffa54488ca4b6a8d59bfe31bba94726
Brandon Keepers <brandon@collectiveidea.com>
2009-06-08 15:35:11 -0400

    Added mailer for outdbid notifications

--- test/unit/bid_mailer_test.rb
+++ test/unit/bid_mailer_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class BidMailerTest < ActionMailer::TestCase
-  # replace this with your real tests
-  test "the truth" do
-    assert true
+  include ActionController::UrlWriter
+  
+  context "outbid_notification" do
+    setup do
+      @listing = Factory(:listing)
+      @bid = Factory(:bid, :amount => 10, :listing => @listing)
+      Factory(:bid, :amount => 11, :listing => @listing)
+      @notification = BidMailer.create_outbid_notification(@bid)
+    end
+    
+    should "send email to bidder" do
+      assert_contains @notification.to, @bid.user.email
+    end
+    
+    should "have a link to the listing" do
+      assert_match listing_path(@listing), @notification.body
+    end
   end
 end
