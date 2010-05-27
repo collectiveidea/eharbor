087f8a3159391b2b89c32b6af5046018411a0870
Daniel Morrison <daniel@collectiveidea.com>
2010-05-27 15:29:09 +0100

    Add bids

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+  has_many :bids
   
   validates_presence_of :title, :description
 end
---
+++ test/unit/bid_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BidTest < ActiveSupport::TestCase
+end
