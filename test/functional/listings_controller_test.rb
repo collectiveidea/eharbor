52e60296f278aba22a0e713eaa293538abadbcc0
karmajunkie <keith@collectiveidea.com>
2010-05-28 14:18:14 -0500

    spec for ListingsController

--- test/functional/listings_controller_test.rb
+++ test/functional/listings_controller_test.rb
@@ -3,10 +3,13 @@ require 'test_helper'
 class ListingsControllerTest < ActionController::TestCase
   
   test "index" do
+    listing = Listing.create! :title => 'Foo', :description => 'Bar'
+    
     get :index
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:listings)
-    assert assigns(:listings).include?(listings(:macbook))
+    assert assigns(:listings).include?(listing)
   end
+  
 end
