e38486363b5d8c62db820ccbc19a6b557b827121
Daniel Morrison <daniel@collectiveidea.com>
2010-10-12 11:15:56 -0400

    Add associations between User and Listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
+  belongs_to :user
+  
   validates_presence_of :title, :description
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  
+
+  has_many :listings
+
   def to_s
     email
   end
