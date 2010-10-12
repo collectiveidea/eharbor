063ca145fd71c7bb32b70e5516fb52f8b04185bd
Daniel Morrison <daniel@collectiveidea.com>
2010-10-12 11:14:00 -0400

    rails generate migration add_user_id_to_listings user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101012145211) do
+ActiveRecord::Schema.define(:version => 20101012151249) do
 
   create_table "listings", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
