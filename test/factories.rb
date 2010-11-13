af5767abb734ae92b30e63c02d919f4c2302b3ef
Daniel Morrison <daniel@collectiveidea.com>
2010-11-13 12:13:02 -0500

    Added a feature to create listing requiring a user to be signed in.

--- test/factories.rb
+++ test/factories.rb
@@ -3,3 +3,9 @@ Factory.define :listing do |f|
   f.description "You can tickle him!"
   f.duration 3
 end
+
+Factory.define :user do |f|
+  f.sequence(:email) {|n| "user#{n}@example.com"}
+  f.password 'password'
+  f.password_confirmation {|user| user.password }
+end
